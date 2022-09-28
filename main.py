03fb9d3ebccbf53037e358d7c5f87771fa35af9b
Jonathan <114219464+Auca-XT@users.noreply.github.com>
2022-09-28 09:03:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#colors 
+#Black   30
+#Red     31
+#Green   32
+#Yellow  33
+#Blue    34
+#Purple  35
+#Cyan    36
+#White   37
+#Default  0
+
+print("Uh, oh," "\033[32m",  "you've", "\033[0m", "been given a",  "\033[31m", "warning", "\033[0m", "for being a", "\033[33m", "bad, bad", "\033[0m", " person.")
